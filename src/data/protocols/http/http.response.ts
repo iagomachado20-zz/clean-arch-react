bba1c5a85a196aca9a5631f31c88ba4c4a101d9d
Iago Machado <machado.iago@gmail.com>
2022-03-23 17:12:59 -0300

    chore: add tests that ensure remoteauthentication trhows

--- src/data/protocols/http/http.response.ts
+++ src/data/protocols/http/http.response.ts
@@ -1,6 +1,10 @@
 export enum HttpStatusCode {
     unathorized = 401,
-    noContent = 204
+    noContent = 204,
+    badRequest = 400,
+    ok = 200,
+    serverError = 500,
+    notFound = 404
 }
 
 export type HttpResponse = {
--- src/data/usecases/authentication/remote.authetication.ts
+++ src/data/usecases/authentication/remote.authetication.ts
@@ -1,6 +1,7 @@
 import { HttpPostClient } from "@/data/protocols/http/http.post.client";
 import { HttpStatusCode } from "@/data/protocols/http/http.response";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-errors";
+import { UnexpectedError } from "@/domain/errors/unexpected-error";
 import { AuthenticationParams } from "@/domain/usecases/authentication";
 
 export class RemoteAuthetication {
@@ -17,8 +18,10 @@ export class RemoteAuthetication {
         });
 
         switch(httpResponse.statusCode) {
+            case HttpStatusCode.ok: break;
             case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
-            default: return Promise.resolve()
+            case HttpStatusCode.badRequest: throw new UnexpectedError()
+            default: throw new UnexpectedError()
         }
 
     }
