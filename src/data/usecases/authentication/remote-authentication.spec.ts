098789b6a04ba624a751fa6592b83274ea966556
Iago Machado <machado.iago@gmail.com>
2022-03-23 17:01:02 -0300

    chore: should HttpResponse returns error trhow 401 InvalidCrendentials error

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,8 @@
 import { RemoteAuthetication } from './remote.authetication';
 import { HttpPostClientSpy } from '../../test/mock-http-client';
 import { mockAuthentication } from '../../../domain/test/mock-authentication';
+import { InvalidCredentialsError } from '../../../domain/errors/invalid-credentials-errors';
+import { HttpStatusCode } from '../../../data/protocols/http/http.response';
 const { faker } = require('@faker-js/faker');
 
 type SutTypes = {
@@ -40,6 +42,21 @@ describe('RemoteAuthetication', () => {
         await sut.auth(authenticationParams);
         
         expect(httpPostClientSpy.body).toEqual(authenticationParams);
+
+
+    })
+
+    test('Should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
+
+        const { sut, httpPostClientSpy } = makeSut();
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.unathorized
+        }
+
+        const promise = sut.auth(mockAuthentication());
+        
+        expect(promise).rejects.toThrow(new InvalidCredentialsError())
 
 
     })
